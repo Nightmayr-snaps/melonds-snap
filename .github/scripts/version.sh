9b031a0a7750b6d84887859f546c22926e5ae962
Nightmayr <umayr23@gmail.com>
2020-10-12 22:27:15 +0100

    initial commit, working snap apart from libpcap

---
+++ .github/scripts/version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# check latest tagged version
+LATEST_VERSION_TAG="$(curl https://api.github.com/repos/Arisotura/melonDS/releases/latest -s | jq .tag_name -r)"
+CURRENT_VERSION_SNAP="$(snap info melonds | grep stable | head -n 2 | tail -n 1 | awk -F ' ' '{print $2}')"
+
+
+# compare versions
+if [ $CURRENT_VERSION_TAG_SNAP != $LATEST_VERSION_TAG ]; then
+    echo "versions don't match, github: $LATEST_VERSION_TAG snap: $CURRENT_VERSION_TAG_SNAP"
+    echo true >> build
+else
+    echo "versions match, github: $LATEST_VERSION_TAG snap: $CURRENT_VERSION_TAG_SNAP"
+    echo false >> build
+fi
+
+
